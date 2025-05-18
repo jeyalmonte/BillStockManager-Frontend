4c24760555e7128842b5343a4d7e7282e4b3a50c
Jeyson Almonte <jjalmonte@humano.com>
2025-05-17 23:04:00 -0400

    refactor: remove Tailwind CSS configuration and styles
    - Deleted Tailwind CSS configuration files (postcss.config.js, tailwind.config.js).
    - Removed Tailwind CSS directives from index.css.
    - Updated App component to display a welcome message.
    - Added environment configuration in a new env.ts file.
    - Created basic structure for authentication feature with Login and Register pages.
    - Introduced empty index files for billing, customers, and inventory features.
    - Updated main.tsx to include Roboto font styles.
    - Adjusted TypeScript configuration for better module resolution and base URL.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+
 export default function App() {
+
   return (
-    <div className="flex items-center justify-center h-screen bg-red-500 text-white">
-      <h1 className="text-5xl font-bold">Hello, Tailwind with Vite!</h1>
-    </div>
+    <h1>Welcome to React + Vite + TypeScript + MUI</h1>
   );
 }
---
+++ src/config/env.ts
@@ -0,0 +1,6 @@
+const env ={
+    API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+    APP_ENV: import.meta.env.VITE_APP_ENV
+}
+
+export default env;
